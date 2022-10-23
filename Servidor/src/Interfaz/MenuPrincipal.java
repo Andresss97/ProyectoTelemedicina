57ef5a818af361c2eae1b11247ba7d514084c580
Andresss97 <ad.pereda@usp.ceu.es>
2022-10-23 14:25:04 +0200

    Servidor

---
+++ Servidor/src/Interfaz/MenuPrincipal.java
@@ -0,0 +1,19 @@
+package Interfaz;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MenuPrincipal extends Application{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
